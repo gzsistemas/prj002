4d1b54a38b0369be70020c78e046e3869ab19a5c
Jonathan Sansalone <jonathan.sansalone@gzsistemas.com.br>
2017-05-03 09:18:42 -0300

    MPRE-5 #comment Ajustes nas paginas HTML. Retirei as chamadas de fontes do Google API, que deixava o aplicativo extremamente lento quando nao conectado a internet. Retirei o botao confirmar e visualizar porque nao tinha acao. #time 1h

--- sinc_git.bat
+++ sinc_git.bat
@@ -1,3 +1,3 @@
 git add *
-git commit --author "Jonathan Sansalone <jonathan.sansalone@gzsistemas.com.br>" -m
+git commit --author "Jonathan Sansalone <jonathan.sansalone@gzsistemas.com.br>"
 git push -u origin master
