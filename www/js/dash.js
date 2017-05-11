3c798de271edeaf8bac87d3d7440f4b85fa78a26
lucas honda <lucas.honda@gzsistemas.com.br>
2017-05-11 17:03:22 -0300

    #comment ajuste na dash para carregar o enderço do servidor

--- www/js/dash.js
+++ www/js/dash.js
@@ -10,7 +10,7 @@ function pesquisa(){
 	//var endServ = storage.getItem("endereco-servidor");
     var endServ = enderecoFormatado();
 	var comanda = $("#txt-com").val();
-	
+
 	if(checarVazio(comanda) != true){
 		toastInfoNoHide("Pesquisando comanda...Aguarde!");
 			$.ajax({
@@ -20,51 +20,58 @@ function pesquisa(){
 				},
 				mensagem: function(resposta){
 					var message = resposta.message;
-					
+
 				},
 				data: {
-									
+
 				},
-				success: function (resposta) {				
+				success: function (resposta) {
 					var isOk = resposta.ok;
 						if(isOk) {
 							var storage = window.localStorage;
 							storage.setItem("comanda", JSON.stringify(comanda));
-							
+
 							var prods = [];
 							prods = resposta.extra.lista_itenscomanda.itemcomanda;
-							
+
 							for(var i = 0; i<prods.length; i++){
-								itsComanda.push(new Produto(i, prods[i].codigoInterno, prods[i].codigoEAN,prods[i].descricao,prods[i].codigoVend,prods[i].tipoPreco,prods[i].quantidade,prods[i].precoUnitario,prods[i].percentualDesconto,prods[i].percentualAcrescimo,prods[i].descontoMultiplo,prods[i].complemento,prods[i].cancelado,prods[i].valorDesconto,prods[i].valorAcrescimo,prods[i].impresso));	
+								itsComanda.push(new Produto(i, prods[i].codigoInterno, prods[i].codigoEAN,prods[i].descricao,prods[i].codigoVend,prods[i].tipoPreco,prods[i].quantidade,prods[i].precoUnitario,prods[i].percentualDesconto,prods[i].percentualAcrescimo,prods[i].descontoMultiplo,prods[i].complemento,prods[i].cancelado,prods[i].valorDesconto,prods[i].valorAcrescimo,prods[i].impresso));
 							}
-							
+
 							var comand = JSON.stringify(itsComanda).replace("[", "").replace("]", "");
-							
-							
+
+
 							storage.setItem("pedidoP"+comanda, comand);
 							storage.setItem("itsComanda", comand);
 							window.location.replace("prevenda.html");
 						}else{
-							bootbox.confirm({ 
+							bootbox.confirm({
                                 title: "Aviso",
                                 message: resposta.mensagem,
-                                size: "small",	
-                                callback: function(result){						  
-                                    
+                                size: "small",
+                                callback: function(result){
+
                                 }
                             });
 						}
 				},
-				error: function (erro) {								
+				error: function (erro) {
 					toastError("Não foi possível pesquisar a comanda!");
-				}				
+				}
 			});
-					
+
 	}else{
 		toastError("Digite uma comanda para continuar!");
 		document.getElementById("txt-com").select();
 	}
 }
+
+function onLoad() {
+	var storage = window.localStorage;
+	var ip = storage.getItem("endereco-servidor")
+	$("#txt-servidor").text(ip);
+}
+
 
 /*
 *	Botão de Pesquisa no dash
